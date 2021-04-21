2b0ad3c5cd39c81b78575576b84be16400047f10
thangchu77 <chuducthang77@gmail.com>
2021-04-21 12:09:59 +0700

    commit

--- CMPUT204/tree.py
+++ CMPUT204/tree.py
@@ -1,5 +1,6 @@
 #1. Breadth First Search
-def BFS(G, s):
+#1.1 BFS 1st implementation used mark array
+def BFS_v1(G, s):
     
     vertex = G[0]
     edges = G[1]
@@ -27,6 +28,90 @@ def BFS(G, s):
                 mark[index] = True
                 queue.append(neighbor)
 
+#1.2 BFS second implementation used color
+class Node:
+    def __init__(self):
+        self.name = None
+        self.color = None
+        self.parent = None
+        self.distance = None
+        self.neighbor = []
+    
+    def get_name(self):
+        return self.name
+    
+    def get_color(self):
+        return self.color
+
+    def get_parent(self):
+        return self.parent
+    
+    def get_distance(self):
+        return self.distance
+    
+    def get_neighbor(self):
+        return self.neighbor
+
+    def set_name(self, name):
+        self.name = name
+    
+    def set_color(self, color):
+        self.color = color
+
+    def set_parent(self, parent):
+        self.parent = parent
+    
+    def set_distance(self,distance):
+        self.distance = distance
+
+    def __str__(self):
+        print(self.name)
+        print(self.color)
+        print(self.parent)
+        #print(self.neighbor)
+        print(self.distance)
+    
+def BFS_v2(G, s):
+    vertex = G[0]
+    edges = G[1]
+
+    # Initialize the empty queue
+    queue = []
+    class_vertex =  []
+    
+    for vertice in vertex:
+        item = Node()
+        item.set_name(vertice)
+        item.set_color('w')
+        #Set the distance to be a big number
+        item.set_distance(99999999)
+        class_vertex.append(item)
+
+    #Set the neighbor nodes
+    for vertice in class_vertex:
+        for edge in edges:
+            if vertice.get_name() == edge[0]:
+                index = vertex.index(edge[1])
+                vertice.get_neighbor().append(class_vertex[index])
+
+    #Choose the node at given index and add to queue
+    item = class_vertex[s]
+    item.set_distance = 0
+    item.set_color = 'g'
+    queue.append(item)
+
+    while len(queue) != 0:
+        choose = queue.pop(0)
+        print(choose.get_name())
+        neighbors = choose.get_neighbor()
+        for neighbor in neighbors:
+            if neighbor.get_color() == 'w':
+                neighbor.set_distance(choose.get_distance() + 1)
+                neighbor.set_parent(choose)
+                neighbor.set_color('g')
+                queue.append(neighbor)
+        
+
 #2. Depth First Search
 
 #3. Depth First Search for Tree/Forest
@@ -37,12 +122,15 @@ def BFS(G, s):
 
 #6. Prim/Dijkstra MST algorithm
 
-
 def main():
     V = ('a','b','c','d','e','f','g')
     E = [('a', 'b'), ('a','d'), ('a','e'), ('b','d'), ('b','e'), ('d','e'), ('a','c'), ('c','f'), ('c','g'),('f','g')]
     G = [V, E]
-    BFS(G, 0)
 
+    #First implementation used BFS v1
+    # BFS_v1(G, 0)
+
+    #Second implementation used class nodes
+    BFS_v2(G,0)
 
 main()
